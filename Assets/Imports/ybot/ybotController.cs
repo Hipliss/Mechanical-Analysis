9232d9911736185152f794009e817d7d9c8c3d23
Hipliss <jxg.yee@gmail.com>
2022-04-20 23:04:18 -0500

    Character Movement

---
+++ Assets/Imports/ybot/ybotController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ybotController : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundMask;
+
+    Animator animator;
+
+    //float speed = 4.0f;
+    //float gravity = -9.81f;
+    //float jumpHeight = 2.0f;
+    float groundDistance = 0.2f;
+
+    //bool isJumping;
+    bool isGrounded;
+
+    Vector3 velocity;
+    
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        controller.slopeLimit = 45;
+        controller.stepOffset = 0.7f;
+    }
+    
+
+    private void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        
+        if (isGrounded && velocity.y <0)
+        {
+            velocity.y = -2f;
+        }
+        
+        /*
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 forward = transform.forward * z;
+        controller.Move(forward * speed * Time.deltaTime);
+        animator.SetFloat("Speed", forward.z);
+
+        Vector3 sides = transform.right * x;
+        controller.Move(sides * speed * Time.deltaTime);
+        animator.SetFloat("Sides", sides.x);
+        */
+        /*
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            controller.slopeLimit = 90;
+            controller.stepOffset = 0;
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            animator.SetBool("Jumping", true);
+            animator.SetBool("Falling", false);
+            animator.SetBool("Grounded", false);
+                        isJumping = true;
+        }
+        else if (isJumping)
+        {
+            animator.SetBool("Jumping", false);
+            animator.SetBool("Falling", true);
+            animator.SetBool("Grounded", false);
+            isJumping = false;
+        }
+        else if (isGrounded)
+        {
+            controller.slopeLimit = 45;
+            controller.stepOffset = 0.7f;
+            animator.SetBool("Jumping", false);
+            animator.SetBool("Falling", false);
+            animator.SetBool("Grounded", true);
+        }
+        */
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            animator.SetTrigger("Jump");
+            //velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        /*
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+        */
+    }
+
+}
---
+++ Assets/Scripts/Comp_CharacterController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HamJoyGames
+{
+    public class Comp_CharacterController : MonoBehaviour
+    {
+
+        [Header("Movement")]
+        [SerializeField] private float _runSpeed = 6f;
+
+        [Header("Sharpness")]
+        [SerializeField] private float _rotationSharpness = 10f;
+        [SerializeField] private float _moveSharpness = 10f;
+
+        private Animator _animator;
+        private Comp_PlayerInputs _inputs;
+        private Comp_CameraController _cameraController;
+
+        private float _targetSpeed;
+        private Quaternion _targetRotation;
+
+        private float _newSpeed;
+        private Vector3 _newVelocity;
+        private Quaternion _newRotation;
+
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+            _inputs = GetComponent<Comp_PlayerInputs>();
+            _cameraController = GetComponent<Comp_CameraController>();
+
+            _animator.applyRootMotion = false;
+        }
+
+        private void Update()
+        {
+            Vector3 _moveInputVector = new Vector3(_inputs.MoveAxisRightRaw, 0, _inputs.MoveAxisForwardRaw).normalized;
+            Vector3 _cameraPlanarDirection = _cameraController.CameraPlanarDirection;
+            Quaternion _cameraPlanarRotation = Quaternion.LookRotation(_cameraPlanarDirection);
+
+            _moveInputVector = _cameraPlanarRotation * _moveInputVector;
+
+            // Move Speed
+            _newSpeed = Mathf.Lerp(_newSpeed, _targetSpeed, Time.deltaTime * _moveSharpness);
+            _targetSpeed = _moveInputVector != Vector3.zero ? _runSpeed : 0;
+
+
+            // Velocity
+            _newVelocity = _moveInputVector * _newSpeed;
+            transform.Translate(_newVelocity * Time.deltaTime, Space.World);
+
+
+            // Rotation
+            if (_targetSpeed != 0)
+            {
+                _targetRotation = Quaternion.LookRotation(_moveInputVector);
+                _newRotation = Quaternion.Slerp(transform.rotation, _targetRotation, Time.deltaTime * _rotationSharpness);
+                transform.rotation = _newRotation;
+            }
+
+            // Animations
+            _animator.SetFloat("Forward", _newSpeed);
+        }
+    }
+}
